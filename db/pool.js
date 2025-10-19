82fb346835eede68f25b85188bb0f4fae4613ce4
tomasbennett <tjs.bennett@gmail.com>
2025-10-19 17:45:53 +0100

    Adding the correct setup in pool.js and queries.js for the Aiven database

--- db/pool.js
+++ db/pool.js
@@ -1,9 +1,14 @@
+require('dotenv').config();
 const { Pool } = require("pg");
 
+const encodedPassword = encodeURIComponent(process.env.AIVEN_DATABASE_PASSWORD);
+
+
 module.exports = new Pool({
-    host: "localhost",
-    user: "tjsbe",
-    database: "top_users",
-    password: process.env.DATABASE_PASSWORD,
-    port: 5432
+    user: process.env.AIVEN_DATABASE_USER,
+    host: process.env.AIVEN_DATABASE_HOST,
+    database: process.env.AIVEN_DATABASE_DATABASE_NAME,
+    password: process.env.AIVEN_DATABASE_PASSWORD,
+    port: Number(process.env.AIVEN_DATABASE_PORT),
+    ssl: { rejectUnauthorized: false }
 });
--- db/queries.js
+++ db/queries.js
@@ -1,12 +1,12 @@
 const pool = require("./pool");
 
 async function getAllUsernames() {
-  const { rows } = await pool.query("SELECT * FROM usernames");
+  const { rows } = await pool.query("SELECT * FROM username_testing_project.usernames");
   return rows;
 }
 
 async function insertUsername(username) {
-  await pool.query("INSERT INTO usernames (username) VALUES ($1)", [username]);
+  await pool.query("INSERT INTO username_testing_project.usernames (username) VALUES ($1)", [username]);
 }
 
 module.exports = {
